65ce277a64a77b38750b4ccdfb7ab392cb60c572
sibi <selvimuthusamy785@gmail.com>
2025-01-01 00:54:06 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,9 +1,13 @@
+ package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
+    @GetMapping("/hello")
+    public String sayHello() {
         return "Hello, World!";
     }
 }
-
