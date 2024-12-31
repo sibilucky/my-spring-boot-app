412eadb9d0d9ed426685720b14032c0edbf4b9a9
sibi <selvimuthusamy785@gmail.com>
2025-01-01 02:16:14 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,16 @@
-package com.example.demo;
+ package com.example.demo.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
-public class DemoApplication {
+@Controller
+public class HomeController {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    @GetMapping("/")
+    public String home(Model model) {
+        // Adding a message to the model, which will be passed to the Thymeleaf template
+        model.addAttribute("message", "Hello, Welcome to My Spring Boot App!");
+        return "index"; // This corresponds to src/main/resources/templates/index.html
     }
 }
-
