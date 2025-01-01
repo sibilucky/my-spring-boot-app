6eb548c350b80aabf4c1b9da44dd99e0578b4cc7
sibi <selvimuthusamy785@gmail.com>
2025-01-01 22:41:50 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,17 +1,7 @@
-// src/main/java/com/example/demo/HomeController.java
-package com.example.demo;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
+@RestController
 public class HomeController {
-
     @GetMapping("/")
     public String home() {
-        // Return the name of the view or static HTML page to be rendered
-        return "index"; // Ensure you have a corresponding index.html in resources/static or resources/templates
+        return "Hello, World!";
     }
 }
-
-
