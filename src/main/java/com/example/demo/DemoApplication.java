76d9e05c60c80aebe6b3f07d82a401681705fda5
sibi <selvimuthusamy785@gmail.com>
2025-01-01 21:49:40 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,4 @@
 // src/main/java/com/example/demo/DemoApplication.java
-
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
@@ -8,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
+        // Print the message "Hi, I am Sibi"
+        System.out.println("Hi, I am Sibi");
+        
+        // Run the Spring Boot application
         SpringApplication.run(DemoApplication.class, args);
     }
 }
